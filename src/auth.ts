555f6312d520bd4cb0b0054b21bf724faa9454b5
山口　流石 <s.yamaguchi@bravesoft.co.jp>
2025-03-01 22:22:46 +0900

    oauth

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,15 @@
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+import { db } from "@/db";
+import { accounts, sessions, users } from "@/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+  }),
+  providers: [Google],
 });
