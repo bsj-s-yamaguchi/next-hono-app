a48e4c2cee5ffe30628b460d091569ed6d928ab2
山口　流石 <s.yamaguchi@bravesoft.co.jp>
2025-03-01 20:01:58 +0900

    initial commit

---
+++ src/components/marketing-header-nav.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export const MarketingHeaderNav = () => {
+  return (
+    <nav>
+      <Button onClick={() => {}}>
+        ログイン
+      </Button>
+    </nav>
+  );
+};
